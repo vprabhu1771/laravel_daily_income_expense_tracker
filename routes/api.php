f35ca586d58a7e7b23106cd800d9c1287c50b9c0
Prabhu <vprabhu1771@gmail.com>
2025-01-03 21:09:05 +0530

    TransactionController api routes added

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+use App\Http\Controllers\api\v2\TransactionController;
+
+Route::prefix('v2/transactions')->group(function () {
+    Route::get('/', [TransactionController::class, 'index']);
+    Route::post('/', [TransactionController::class, 'store']);
+});
