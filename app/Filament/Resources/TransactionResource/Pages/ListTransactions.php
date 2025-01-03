137fd153ae94e27793ee05be5eb3e7438ed59fe4
Prabhu <vprabhu1771@gmail.com>
2025-01-03 17:25:30 +0530

    TransactionResource All, Income, Expense Tab filter added

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -6,6 +6,11 @@ use App\Filament\Resources\TransactionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
+use App\Enums\TransactionType;
+
 class ListTransactions extends ListRecords
 {
     protected static string $resource = TransactionResource::class;
@@ -14,6 +19,17 @@ class ListTransactions extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+ 
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'Income' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TransactionType::INCOME)),
+            'Expense' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TransactionType::EXPENSE)),
         ];
     }
 }
