c2e8bb3db71c8895a96542a1f4a385f118c81240
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-04 00:13:40 +0530

    ReportController today routes added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ReportController;
+
+Route::get('/report/today', [ReportController::class, 'generateTodayReport'])->name('report.today');
+Route::get('/report/today/download', [ReportController::class, 'downloadTodayReport'])->name('report.today.download');
